0914df64758eea13af37cc8d86a04141ee8d19a5
Ben Chaplin <benchaplin@protonmail.ch>
2022-12-03 22:37:23 -0500

    day 2 and 3

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 fn main() {
     let input = include_str!("../../inputs/02.txt");
     pt_one(input);
@@ -5,7 +7,61 @@ fn main() {
 }
 
 fn pt_one(input: &str) {
+    println!("{:?}", input.split('\n')
+        .map(|line| {
+            let moves: Vec<&str> = line.split(' ').collect();
+            let elf_val = match moves[0] {
+                "A" => { 1 } // rock
+                "B" => { 2 } // paper
+                "C" => { 3 } // scissors
+                &_ => { panic!("bad val") }
+            };
+            let my_val = match moves[1] {
+                "X" => { 1 } // rock
+                "Y" => { 2 } // paper
+                "Z" => { 3 } // scissors
+                &_ => { panic!("bad val") }
+            };
+
+            let mut score = my_val;
+            if (my_val - elf_val + 3) % 3 == 1 {
+                score += 6;
+            } else if my_val == elf_val {
+                score += 3;
+            }
+            
+            score
+        })
+        .sum::<i32>());
 }
 
 fn pt_two(input: &str) {
+    println!("{:?}", input.split('\n')
+        .map(|line| {
+            let moves: Vec<&str> = line.split(' ').collect();
+            let elf_val = match moves[0] {
+                "A" => { 1 } // rock
+                "B" => { 2 } // paper
+                "C" => { 3 } // scissors
+                &_ => { panic!("bad val") }
+            };
+            let my_res = match moves[1] {
+                "X" => { 0 } // lose
+                "Y" => { 3 } // draw 
+                "Z" => { 6 } // win 
+                &_ => { panic!("bad val") }
+            };
+
+            let mut score = my_res;
+            if my_res == 0 {
+                score += ((elf_val - 1) + 2) % 3 + 1;
+            } else if my_res == 3 {
+                score += elf_val;
+            } else {
+                score += ((elf_val - 1) + 1) % 3 + 1;
+            }
+            
+            score
+        })
+        .sum::<i32>());
 }
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,11 +1,53 @@
+use std::collections::HashSet;
+use itertools::Itertools;
+
 fn main() {
     let input = include_str!("../../inputs/03.txt");
     pt_one(input);
     pt_two(input);
 }
 
+fn get_char_val(c: char) -> u32 {
+    if c.is_uppercase() {
+        return c as u32 - 64 + 26;
+    }
+    c as u32 - 96
+}
+
 fn pt_one(input: &str) {
+    println!("{:?}", input.split('\n')
+        .map(|line| {
+            let first_half: HashSet<char> = line.chars()
+                .enumerate()
+                .filter(|&(i, _)| i < line.len() / 2)
+                .map(|(_, e)| e)
+                .collect();
+            let second_half: HashSet<char> = line.chars()
+                .enumerate()
+                .filter(|&(i, _)| i >= line.len() / 2)
+                .map(|(_, e)| e)
+                .collect();
+            
+            let common = &first_half & &second_half;
+
+            get_char_val(common.iter().next().unwrap().clone())
+        })
+        .sum::<u32>()
+    );
 }
 
 fn pt_two(input: &str) {
+    println!("{:?}", input.split('\n')
+        .tuples()
+        .map(|(line1, line2, line3)| {
+            let line1: HashSet<char> = line1.chars().collect();
+            let line2: HashSet<char> = line2.chars().collect();
+            let line3: HashSet<char> = line3.chars().collect();
+
+            let common = &(&line1 & &line2) & &line3; 
+
+            get_char_val(common.iter().next().unwrap().clone())
+        })
+        .sum::<u32>()
+    );
 }
