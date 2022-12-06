f10fe13e958ea8214fefae00274ed92eb7cfbcbf
Ben Chaplin <benchaplin@protonmail.ch>
2022-12-06 16:25:19 -0500

    day 4 and 5

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -5,7 +5,43 @@ fn main() {
 }
 
 fn pt_one(input: &str) {
+    println!("{:?}", input.split('\n')
+        .map(|line| {
+            let ranges: Vec<&str> = line.split(',').collect();
+
+            let first_min = ranges[0].split('-').collect::<Vec<&str>>()[0].parse::<u32>().unwrap();
+            let first_max = ranges[0].split('-').collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
+            let second_min = ranges[1].split('-').collect::<Vec<&str>>()[0].parse::<u32>().unwrap();
+            let second_max = ranges[1].split('-').collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
+
+            (first_min, first_max, second_min, second_max)
+        })
+        .filter(|(first_min, first_max, second_min, second_max)| 
+            (first_min <= second_min && first_max >= second_max) ||
+            (second_min <= first_min && second_max >= first_max)
+        )
+        .count()
+    );
 }
 
 fn pt_two(input: &str) {
+    println!("{:?}", input.split('\n')
+        .map(|line| {
+            let ranges: Vec<&str> = line.split(',').collect();
+
+            let first_min = ranges[0].split('-').collect::<Vec<&str>>()[0].parse::<u32>().unwrap();
+            let first_max = ranges[0].split('-').collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
+            let second_min = ranges[1].split('-').collect::<Vec<&str>>()[0].parse::<u32>().unwrap();
+            let second_max = ranges[1].split('-').collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
+
+            (first_min, first_max, second_min, second_max)
+        })
+        .filter(|(first_min, first_max, second_min, second_max)| {
+            (second_min >= first_min && second_min <= first_max) ||
+            (second_max >= first_min && second_max <= first_max) ||
+            (first_min <= second_min && first_max >= second_max) ||
+            (second_min <= first_min && second_max >= first_max)
+        })
+        .count()
+    );
 }
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,11 +1,80 @@
 fn main() {
     let input = include_str!("../../inputs/05.txt");
     pt_one(input);
+    println!("");
     pt_two(input);
 }
 
 fn pt_one(input: &str) {
+    let s1 = vec!['C', 'Z', 'N', 'B', 'M', 'W', 'Q', 'V'];
+    let s2 = vec!['H', 'Z', 'R', 'W', 'C', 'B'];
+    let s3 = vec!['F', 'Q', 'R', 'J'];
+    let s4 = vec!['Z', 'S', 'W', 'H', 'F', 'N', 'M', 'T'];
+    let s5 = vec!['G', 'F', 'W', 'L', 'N', 'Q', 'P'];
+    let s6 = vec!['L', 'P', 'W'];
+    let s7 = vec!['V', 'B', 'D', 'R', 'G', 'C', 'Q', 'J'];
+    let s8 = vec!['Z', 'Q', 'N', 'B', 'W'];
+    let s9 = vec!['H', 'L', 'F', 'C', 'G', 'T', 'J'];
+    let mut stacks: Vec<Vec<char>> = vec![s1, s2, s3, s4, s5, s6, s7, s8, s9];
+
+    let instructions = input.split('\n')
+        .skip(10)
+        .map(|line| {
+            let split: Vec<&str> = line.split(' ').collect();
+            let move_num = split[1].parse::<usize>().unwrap();
+            let from = split[3].parse::<usize>().unwrap();
+            let to = split[5].parse::<usize>().unwrap();
+
+            (move_num, from, to)
+        });
+
+    for (move_num, from, to) in instructions {
+        for _ in 0..move_num {
+            let creight = stacks[from - 1].pop().unwrap();
+            stacks[to - 1].push(creight);
+        }
+    }
+
+    for stack in stacks {
+        print!("{}", stack.get(stack.len() - 1).unwrap());
+    }
 }
 
 fn pt_two(input: &str) {
+    let s1 = vec!['C', 'Z', 'N', 'B', 'M', 'W', 'Q', 'V'];
+    let s2 = vec!['H', 'Z', 'R', 'W', 'C', 'B'];
+    let s3 = vec!['F', 'Q', 'R', 'J'];
+    let s4 = vec!['Z', 'S', 'W', 'H', 'F', 'N', 'M', 'T'];
+    let s5 = vec!['G', 'F', 'W', 'L', 'N', 'Q', 'P'];
+    let s6 = vec!['L', 'P', 'W'];
+    let s7 = vec!['V', 'B', 'D', 'R', 'G', 'C', 'Q', 'J'];
+    let s8 = vec!['Z', 'Q', 'N', 'B', 'W'];
+    let s9 = vec!['H', 'L', 'F', 'C', 'G', 'T', 'J'];
+    let mut stacks: Vec<Vec<char>> = vec![s1, s2, s3, s4, s5, s6, s7, s8, s9];
+
+    let instructions = input.split('\n')
+        .skip(10)
+        .map(|line| {
+            let split: Vec<&str> = line.split(' ').collect();
+            let move_num = split[1].parse::<usize>().unwrap();
+            let from = split[3].parse::<usize>().unwrap();
+            let to = split[5].parse::<usize>().unwrap();
+
+            (move_num, from, to)
+        });
+
+    for (move_num, from, to) in instructions {
+        let from_stack = stacks[from - 1].clone();
+        let creights = &from_stack[(from_stack.len() - move_num)..from_stack.len()];
+        for _ in 0..move_num {
+            stacks[from - 1].pop();
+        }
+        for creight in creights {
+            stacks[to - 1].push(creight.clone());
+        }
+    }
+
+    for stack in stacks {
+        print!("{}", stack.get(stack.len() - 1).unwrap());
+    }
 }
