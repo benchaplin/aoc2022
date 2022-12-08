3de32b400185325e3aa711a1aa93ad7a2409f562
Ben Chaplin <benchaplin@protonmail.ch>
2022-12-07 19:54:48 -0500

    day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("../../inputs/06.txt");
     pt_one(input);
@@ -5,7 +7,29 @@ fn main() {
 }
 
 fn pt_one(input: &str) {
+    let chars: Vec<char> = input.chars().collect();
+
+    for i in 3..chars.len() {
+        let last_four: HashSet<&char> = HashSet::from_iter(
+            &chars[(i - 3)..(i + 1)]
+        );
+        if last_four.len() == 4 {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
 
 fn pt_two(input: &str) {
+    let chars: Vec<char> = input.chars().collect();
+
+    for i in 13..chars.len() {
+        let last_fourteen: HashSet<&char> = HashSet::from_iter(
+            &chars[(i - 13)..(i + 1)]
+        );
+        if last_fourteen.len() == 14 {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
